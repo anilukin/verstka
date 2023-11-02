822d1f62b396a9e9f60a9dd9096021f6f646931c
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2023-11-02 22:50:02 +0100

    add DocumentInfo component

--- src/app/DocumentInfo/DocumentInfo.js
+++ src/app/DocumentInfo/DocumentInfo.js
@@ -1,9 +1,66 @@
 import './documentInfo.css';
 
-export default function DocumentInfo() {
+export default function DocumentInfo({data}) {
     return (
         <div className="documentDetails_info">
-            <h4>Document details</h4>
+            <h3>Document details</h3>
+            <div className="documentDetails_info_container">
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Name:</p>
+                    <p className="documentDetails_info_data">{data.name}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Created on:</p>
+                    <p className="documentDetails_info_data">{data.creatingDate}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Pages:</p>
+                    <p className="documentDetails_info_data">{data.pagesCount}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Created by:</p>
+                    <p className="documentDetails_info_data">{data.author}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Location:</p>
+                    <p className="documentDetails_info_data">{data.location}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Size:</p>
+                    <p className="documentDetails_info_data">{data.size}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Type:</p>
+                    <p className="documentDetails_info_data">{data.type}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">Retention date:</p>
+                    <p className="documentDetails_info_data">{data.retentionDate}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">META DATA:</p>
+                    <p className="documentDetails_info_data">{data.metaData}</p>
+                </div>
+
+                <div className="documentDetails_info_card">
+                    <p className="documentDetails_info_label">SHORTCUTS:</p>
+                    {data.shortcuts.map((link, i) => {
+                        return (
+                            <p key={link.id} className="documentDetails_info_data">
+                                <a href="{link}">Shortcut {i+1} (is link to shortcut)</a>
+                            </p>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 };
