32fd1e4ec2c5bac24daf9854709c6fe17407b964
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2023-11-02 17:37:56 +0100

    add DocumentInfo component

---
+++ src/app/DocumentInfo/DocumentInfo.js
@@ -0,0 +1,9 @@
+import './documentInfo.css';
+
+export default function DocumentInfo() {
+    return (
+        <div className="documentDetails_info">
+            <h4>Document details</h4>
+        </div>
+    );
+};
