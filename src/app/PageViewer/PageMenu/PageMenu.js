e5dee8304bd3903f0b6d3166af21f3112e9844a0
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2023-11-03 11:20:36 +0100

    add PageMenu component

---
+++ src/app/PageViewer/PageMenu/PageMenu.js
@@ -0,0 +1,36 @@
+import './pageMenu.css';
+
+import Image from 'next/image'
+
+import IconPrint from '../../../../public/page-menu-icon-print.svg';
+import IconShare from '../../../../public/page-menu-icon-share.svg';
+import IconCopy from '../../../../public/menu-icon2.svg';
+import NavIcon from '../../../../public/nav-icon.svg';
+
+export default function PageMenu() {
+
+    return (
+        <div className="pageMenu_container">
+            <div className="pageMenu_container_mainOptions">
+                <div className="pageMenu_container_option">
+                    <Image src={IconPrint} alt="icon-print" width={16} height={16} />
+                    <span>Print</span>
+                </div>
+                <div className="pageMenu_container_option">
+                    <Image src={IconShare} alt="icon-print" width={16} height={16} />
+                    <span>Share</span>
+                </div>
+                <div className="pageMenu_container_option">
+                    <Image src={IconCopy} alt="icon-print" width={16} height={16} />
+                    <span>Copy</span>
+                </div>
+            </div>
+            <div className="pageMenu_container_moreOptions">
+                <div className="pageMenu_container_option">
+                    <Image src={NavIcon} alt="icon-print" width={16} height={16} />
+                    <span>More</span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/PageViewer/PageViewer.js
+++ src/app/PageViewer/PageViewer.js
@@ -1,11 +1,12 @@
 import './pageViewer.css';
 
-
+import PageMenu from './PageMenu/PageMenu';
 
 export default function PageViewer() {
 
     return (
         <div className="pageViewer_container">
+            <PageMenu />
             Hello!
         </div>
     );
