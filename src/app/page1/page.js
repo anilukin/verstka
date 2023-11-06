a9eef53f90b44ada2f991d3744c0ef210e044d03
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2023-11-06 22:19:52 +0100

    added page1 and page2

---
+++ src/app/page1/page.js
@@ -0,0 +1,12 @@
+import './page.css'
+
+// import Header from './Header/Header';
+// import Sidebar from './Sidebar/Sidebar';
+// import PageViewer from './PageViewer/PageViewer';
+// import DocumentDetails from './DocumentDetails/DocumentDetails';
+
+export default function Page1() {
+  return (
+    <div>page1</div>
+  )
+}
---
+++ src/app/page2/page.js
@@ -0,0 +1,12 @@
+import './page.css'
+
+// import Header from './Header/Header';
+// import Sidebar from './Sidebar/Sidebar';
+// import PageViewer from './PageViewer/PageViewer';
+// import DocumentDetails from './DocumentDetails/DocumentDetails';
+
+export default function Page2() {
+  return (
+    <div>page2</div>
+  )
+}
